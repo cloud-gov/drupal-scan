3aa105d1afb733c7391770e6b3bc35fec14c9a1e
Mark Headd <mheadd@gmail.com>
2021-01-04 16:25:09 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Scanning for Drupal 
+
+A script to scan federal domains to identify sites running Drupal. This script uses data from [this GSA repository](https://github.com/GSA/data). The script makes a `curl` request to a website URL and looks for the `X-Generator` tag to identify Drupal sites. (Note - this is not a bulletproof way of identifying Drupal sites, but it's common for Drupal sites to use this response header.)
+
+## Usage
+
+Modify the default settings if needed. Invoke the script and direct output to a file, like so:
+
+```bash
+~$ ./scan.sh > site-generators.csv
+```
+
+## Other sources of similar data
+
+* [Analysis of federal .gov domains](https://ben.balter.com/2021-analysis-of-federal-dotgov-domains/) 
---
+++ scan.sh
@@ -0,0 +1,31 @@
+##!/bin/bash
+
+# Default value for site geneator if none found by scan.
+default="Unknown"
+
+# file to hold list of all federal domains.
+file=current-federal.csv
+
+# Timeout values for curl command.
+max_time=10
+connect_time=5
+
+# Download current listing of federal domains for Executive agencies, if needed.
+if [ ! -f "$file" ]; then
+    curl -s https://raw.githubusercontent.com/GSA/data/master/dotgov-domains/current-federal.csv \
+    | grep "Federal Agency - Executive" \
+    > current-federal.csv
+fi
+
+# Get domains to curl.
+domains=($(cat $file | csvcut -c 1 | sort))
+
+# Header row.
+echo "Domain, Generator"
+
+# curl domains and extract the x-generator HTTP response header.
+for domain in "${domains[@]}"
+do
+    generator=$(curl -sIL --max-time ${max_time} --connect-timeout ${connect_time} $domain | grep -i x-generator)
+    echo "${domain}, ${generator:-$default}"
+done
